ab95b52473edc827ba5f47c71fac65eefc04b49e
Sophian YOUSSEFI <sophian.youssefi@epitech.eu>
2021-10-12 23:07:53 +0200

    Updates

---
+++ precache-manifest.b44bc91ca344334fe4ea8667051e2e07.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b30f8a5b4e0c4ea0b4131062f2162f8",
+    "url": "/FacelessFigh/index.html"
+  },
+  {
+    "revision": "a575910556b42365c845",
+    "url": "/FacelessFigh/static/css/main.6d302f0d.chunk.css"
+  },
+  {
+    "revision": "99c99f9ac35298e34f50",
+    "url": "/FacelessFigh/static/js/2.9dde06c7.chunk.js"
+  },
+  {
+    "revision": "ac14a138c0c477165da30e14493a3571",
+    "url": "/FacelessFigh/static/js/2.9dde06c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a575910556b42365c845",
+    "url": "/FacelessFigh/static/js/main.07dbd398.chunk.js"
+  },
+  {
+    "revision": "20188a0bb5a1ba353248",
+    "url": "/FacelessFigh/static/js/runtime-main.f0b7777e.js"
+  },
+  {
+    "revision": "03e3010301bb9960988c34efe6422e96",
+    "url": "/FacelessFigh/static/media/Witcher.03e30103.png"
+  },
+  {
+    "revision": "8d1d044de29599d3497ec6b0ab39abb7",
+    "url": "/FacelessFigh/static/media/assassin1.8d1d044d.png"
+  },
+  {
+    "revision": "a31a43269717937b31694a16f9058600",
+    "url": "/FacelessFigh/static/media/assassin2.a31a4326.png"
+  },
+  {
+    "revision": "5db4f886c4fc39cc563ebabdd0bcc9a2",
+    "url": "/FacelessFigh/static/media/assassin3.5db4f886.png"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/FacelessFigh/static/media/map-image.f40ed5b1.png"
+  }
+]);
